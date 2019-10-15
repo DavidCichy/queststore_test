7bf4c6ca9391588d99fa3d741a234381edf0b77f
David <cichy.dawid@gmail.com>
2019-10-15 10:54:45 +0200

    Test setup

---
+++ src/tests/java/com/codecool/DAO/DatabaseConnectionTest.java
@@ -0,0 +1,10 @@
+package com.codecool.DAO;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseConnectionTest {
+
+    @org.junit.jupiter.api.Test
+    void setConnection() {
+    }
+}
