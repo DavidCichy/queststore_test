480c6a5eceacad0155488e145fb3eaf7a6c4e119
David <cichy.dawid@gmail.com>
2019-10-15 13:09:07 +0200

    DatabaseConnection tests

--- src/main/java/com/codecool/DAO/DatabaseConnection.java
+++ src/main/java/com/codecool/DAO/DatabaseConnection.java
@@ -5,11 +5,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
+
     public Connection setConnection() {
+        Connection connection = setConnection("jdbc:postgresql://localhost:5432/QuestStore", "postgres", "fallout");
+        return connection;
+    }
+
+    public Connection setConnection(String db, String user, String password) {
         Connection connection = null;
         try {
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/QuestStore", "postgres",
-                    "fallout");
+            connection = DriverManager.getConnection(db, user, password);
             connection.setAutoCommit(false);
         } catch (SQLException e) {
             System.err.println("SQLException: " + e.getMessage()
--- src/tests/java/com/codecool/DAO/DatabaseConnectionTest.java
+++ src/tests/java/com/codecool/DAO/DatabaseConnectionTest.java
@@ -1,10 +1,46 @@
 package com.codecool.DAO;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DatabaseConnectionTest {
+    DatabaseConnection dbconnection;
+
+
+    @BeforeEach
+    void setUp() {
+        this.dbconnection = new DatabaseConnection();
+    }
 
     @org.junit.jupiter.api.Test
-    void setConnection() {
+    void checkIfDefaultConnectionIsValid() throws SQLException {
+        Connection connection = dbconnection.setConnection();
+        assertEquals(true, connection.isValid(100));
+    }
+
+    @org.junit.jupiter.api.Test
+    void checkIfCorrectConnectionIsValid() throws SQLException {
+        Connection connection = dbconnection.setConnection("jdbc:postgresql://localhost:5432/QuestStore", "postgres", "fallout");
+        assertEquals(true, connection.isValid(100));
+    }
+
+    @org.junit.jupiter.api.Test
+    void checkIsConnectionNullIfDBDataIsInvalid()   throws SQLException {
+        assertNull(dbconnection.setConnection("jdbc:postgresql://localhost:5432/wrongdatabase/", "postgres", "fallout"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void checkIsConnectionNullIfUserIsInvalid()   throws SQLException {
+        assertNull(dbconnection.setConnection("jdbc:postgresql://localhost:5432/QuestStore", "possdsdtgres", "fallout"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void checkIsConnectionNullIfPasswordIsInvalid()   throws SQLException {
+        assertNull(dbconnection.setConnection("jdbc:postgresql://localhost:5432/QuestStore", "postgres", "fasdfsgllout"));
     }
 }
